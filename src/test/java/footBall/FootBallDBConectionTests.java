e296d664afec7f2ed61fe5041036b04bd0a2aae5
jjackchoi <jackchoi0325@gmail.com>
2024-01-02 15:17:21 +0900

    db 연결 test 코드 추가

---
+++ src/test/java/footBall/FootBallDBConectionTests.java
@@ -0,0 +1,43 @@
+package footBall;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+public class FootBallDBConectionTests {
+    @Autowired
+    private ApplicationContext context;
+
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+
+    @Test
+    void contextLoads(){
+
+    }
+
+    @Test
+    public void testByApplicationContext(){
+        try{
+            System.out.println("======================");
+            System.out.println(context.getBean("sqlSessionFactory"));
+            System.out.println("======================");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testBySqlSessionFactory(){
+        try{
+            System.out.println("================");
+            System.out.println(sqlSessionFactory.toString());
+            System.out.println("================");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

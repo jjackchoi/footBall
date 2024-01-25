1172a764af31fa01cb2559c879cc5b2343a920d5
parkhyenwoo <hyenwoo05@gmail.com>
2024-01-25 17:17:17 +0900

    화면추가

--- src/main/java/footBall/user/UserController.java
+++ src/main/java/footBall/user/UserController.java
@@ -1,8 +1,17 @@
 package footBall.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+    @GetMapping("/")
+    public String register(){
+        return "register";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
 
 }

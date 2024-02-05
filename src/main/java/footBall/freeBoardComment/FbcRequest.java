ec776dff2c9063a8fde5508676b1c5e2d8d99ad9
parkhyenwoo <hyenwoo05@gmail.com>
2024-02-05 13:22:40 +0900

    댓글dto 수정

--- src/main/java/footBall/freeBoardComment/FbcRequest.java
+++ src/main/java/footBall/freeBoardComment/FbcRequest.java
@@ -6,8 +6,8 @@ import lombok.Data;
 public class FbcRequest {
 
     private int fbcId; // 댓글 ID
-    private int free_board_id; // 해당 댓글의 게시글 ID
-    private String fbc_author; // 댓글 작성자
-    private String fbc_text; // 댓글 내용
+    private int freeBoardId; // 해당 댓글의 게시글 ID
+    private String fbcAuthor; // 댓글 작성자
+    private String fbcText; // 댓글 내용
 
 }

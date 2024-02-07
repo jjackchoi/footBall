b829583d7e012a0a8649d7ddd8f2f52f49fb5bec
jjackchoi <jackchoi0325@gmail.com>
2024-02-08 00:35:10 +0900

    건의게시판 쿼리 추가

--- src/main/resources/sql 모음/suggestion_board 수정 모음.sql
+++ src/main/resources/sql 모음/suggestion_board 수정 모음.sql
@@ -1,2 +1,5 @@
 ALTER TABLE football.suggestion_board ADD SUGGESTION_BOARD_RESOLUTION varchar(2) NOT NULL COMMENT '건의게시판 해결여부';
 ALTER TABLE football.suggestion_board CHANGE SUGGESTION_BOARD_RESOLUTION SUGGESTION_BOARD_SOL_YN varchar(2) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '건의게시판 해결여부';
+select *
+from suggestion_board
+where suggestion_board.SUGGESTION_BOARD_DEL_YN = 'N';

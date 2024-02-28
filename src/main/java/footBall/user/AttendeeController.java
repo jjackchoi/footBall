ff8317638054646e28e046e4434ff72d014a6d72
jjackchoi <jackchoi0325@gmail.com>
2024-02-28 16:52:05 +0900

    셀레니움 환경설정 중

---
+++ src/main/java/footBall/user/AttendeeController.java
@@ -0,0 +1,20 @@
+package footBall.user;
+
+import org.apache.ibatis.annotations.Arg;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
+
+@Controller
+public class AttendeeController {
+    public static void main(String[] args) {
+
+
+    }
+
+}

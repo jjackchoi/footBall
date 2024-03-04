580d3fec6d498b4a64950502abe8f5ace0692ba7
jjackchoi <jackchoi0325@gmail.com>
2024-03-04 16:59:13 +0900

    금주의 참석인원 구현중3

--- src/main/java/footBall/user/AttendeeController.java
+++ src/main/java/footBall/user/AttendeeController.java
@@ -1,15 +1,22 @@
 package footBall.user;
 
-import org.apache.ibatis.annotations.Arg;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class AttendeeController {
+
+    @Autowired
+    private UserServiceImpl userService;
+
     @GetMapping("/weeklyAttendee")
-    public String weeklyAttendee(){
+    public String weeklyAttendee(Model model){
+        List<UserResponse> users = userService.getAllUser();
+        model.addAttribute("users", users);
         return "weeklyAttendee/weeklyAttendee";
     }
 

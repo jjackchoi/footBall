c64b2e6ab59372266e519f6103527c1c1b5c579d
jjackchoi <jackchoi0325@gmail.com>
2024-03-25 18:43:13 +0900

    ui 디자인 구현중4 (공지사항 적용 및 게시판 높이 수정)

--- src/main/resources/sql 모음/fb_user 수정 모음.sql
+++ src/main/resources/sql 모음/fb_user 수정 모음.sql
@@ -1,3 +1,3 @@
 ALTER TABLE football.fb_user MODIFY COLUMN FB_USER_PHONE varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL NULL COMMENT '유저 휴대폰번호';
-/*유저권한 칼럼 추가*/
-ALTER TABLE football.fb_user ADD FB_USER_IS_ADMIN varchar(2) DEFAULT 'N' NOT NULL COMMENT '유저 권한';
+/*멤버여부 칼럼 추가*/
+ALTER TABLE football.fb_user ADD FB_USER_MEMBER_YN varchar(2) NOT NULL COMMENT '유저 멤버여부';
